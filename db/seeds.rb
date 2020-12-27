8f05cc7deb9363621fd194ddd9f2aaec27f6a887
kohana__11 <k.ohana@hotmail.co.jp>
2020-12-28 08:52:00 +0900

    input initial data

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,16 +2,11 @@ require "csv"
 
 CSV.foreach('db/transportation.csv') do |row|
   Transportation.create(
-  :name_ja => row[0],
-  :name_en => row[1],
-  :category_ja => row[2],
-  :category_en => row[3],
+  :category1_ja => row[0],
+  :category1_en => row[1],
+  :category2_ja => row[2],
+  :category2_en => row[3],
   :description_ja => row[4],
-  :price_ja => row[6],
-  :price_en => row[7],
-  :airport_town => row[8],
-  :between_town => row[9],
-  :between_island => row[10],
-  :url => row[11]
+  :description_en => row[5],
   )
 end
