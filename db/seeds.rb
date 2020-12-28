286a7fe34519cd0702dd00771f70eaeffb1b6f0a
kohana__11 <k.ohana@hotmail.co.jp>
2020-12-28 09:24:15 +0900

    input first data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 require "csv"
 
-CSV.foreach('db/speciality.csv') do |row|
-  Speciality.create(:name_ja => row[0], :name_en => row[1], :category_ja => row[2], :category_en => row[3], :category_sub_ja => row[4], :category_sub_en => row[5], :alchohol => row[6], :description_ja => row[7])
+CSV.foreach('db/restaurant.csv') do |row|
+  Restaurant.create(:name_ja => row[0], :name_en => row[1], :category_ja => row[2], :category_en => row[3], :description_ja => row[4], :holidays_ja => row[6], :holidays_en => row[7], :businesshours_ja => row[8], :businesshours_en => row[9], :area_ja => row[12], :area_en => row[13], :address => row[14], :mapurl => row[15], :url => row[16])
 end
