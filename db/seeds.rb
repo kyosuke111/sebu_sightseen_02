05d9765431111cd2ebc0edea187fd008bf810c39
kohana__11 <k.ohana@hotmail.co.jp>
2020-12-28 10:16:06 +0900

    input first data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,20 @@
 require "csv"
 
-CSV.foreach('db/localbrand.csv') do |row|
-  Localbrand.create(:name_ja => row[0], :name_en => row[1], :category_ja => row[2], :category_en => row[3], :description_ja => row[4], :url => row[14])
+CSV.foreach('db/sightseen.csv') do |row|
+  Sightseen.create(:name_ja => row[0],
+  :name_en => row[1],
+  :category_ja => row[2],
+  :category_en => row[3],
+  :description_ja => row[4],
+  :holidays_ja => row[6],
+  :holidays_en => row[7],
+  :businesshours_ja => row[8],
+  :businesshours_en => row[9],
+  :price_ja => row[10],
+  :price_en => row[11],
+  :area_ja => row[12],
+  :area_en => row[13],
+  :address => row[14],
+  :url => row[15],
+  :mapurl => row[22])
 end
